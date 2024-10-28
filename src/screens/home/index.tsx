aaa18d57294c6b32d1e24b428a4b708b543f435d
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-10-28 11:21:23 -0300

    feat: add Button component with styled variants and container styles.

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "../../components/button";
 import { Logo } from "../../components/logo";
 import { Header } from "./styles";
 
@@ -6,6 +7,10 @@ export function Home() {
     return (
         <Header>
             <Logo />
+            <div>
+                <Button variant="default">Nova transação</Button>
+                <Button variant="default">Nova categoria</Button>
+            </div>
         </Header>
     )
 }
--- src/screens/home/styles.ts
+++ src/screens/home/styles.ts
@@ -6,4 +6,11 @@ align-items: center;
 justify-content: space-between;
 padding: 1.5rem;
 width: 100%;
+
+>div{
+    display:flex ;
+    align-items: center;
+    gap: 0.5rem;
+
+}
 `
