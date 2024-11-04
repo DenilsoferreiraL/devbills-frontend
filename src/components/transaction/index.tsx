22bae7fad4df57086a2e72e55509613fc91a57d1
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-11-04 10:37:23 -0300

    feat: add Info and Content components with transaction details and formatting.

--- src/components/transaction/index.tsx
+++ src/components/transaction/index.tsx
@@ -1,9 +1,21 @@
-import { Container } from "./styles";
+import { FormatCurrency } from "../../utils/format-currency";
+import { Container, Info, Content } from "./styles";
 
 export function Transaction() {
     return (
         <Container>
+            <Info>
+                <span>#000</span>
+                <div>
+                    <strong>Mercado</strong>
+                    <span>18/01/2000</span>
+                </div>
+            </Info>
 
+            <Content>
+                <strong>{FormatCurrency(10000)}</strong>
+                <span>ALIMENTAÇÃO</span>
+            </Content>
         </Container>
     )
 }
