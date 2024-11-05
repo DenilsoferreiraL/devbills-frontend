693dc0da6e9a458243d4527a6841620b7db7f408
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-11-05 09:26:36 -0300

    feat: add handleClose and onSubmit callbacks with button actions in CreateCategoryDialog.

--- src/components/create-category-dialog/index.tsx
+++ src/components/create-category-dialog/index.tsx
@@ -1,19 +1,20 @@
 import { Container } from "./styles";
 import { Dialog } from "../dialog";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { Button } from "../button";
 import { Title } from "../title";
 import { Input } from "../input";
 
-// type DialogProps = {
-//     children: ReactNode;
-//     trigger: JSX.Element;
-//     open?: boolean;
-//     onOpenChange?: (open: boolean) => void
-// }
-
 export function CreateCategoryDialog() {
     const [open, setOpen] = useState(false)
+
+    const handleClose = useCallback(() => {
+        setOpen(false)
+    }, [])
+
+    const onSubmit = useCallback(() => {
+        handleClose()
+    }, [handleClose])
 
     return (
         <Dialog open={open} onOpenChange={setOpen} trigger={<Button>Nova categoria</Button>}>
@@ -25,8 +26,8 @@ export function CreateCategoryDialog() {
                         <Input label="Cor" type="color" style={{ cursor: "pointer" }} />
                     </div>
                     <footer>
-                        <Button variant="outline" type="button">Cancelar</Button>
-                        <Button type="button">Cadastrar</Button>
+                        <Button onClick={handleClose} variant="outline" type="button">Cancelar</Button>
+                        <Button onClick={onSubmit} type="button">Cadastrar</Button>
                     </footer>
                 </form>
             </Container>
