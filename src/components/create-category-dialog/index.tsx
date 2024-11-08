6134e0d854c5c85244c23e8030014c3831978f68
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-11-08 10:30:44 -0300

    feat(form): add loading state to submit button to prevent multiple submissions.

--- src/components/create-category-dialog/index.tsx
+++ src/components/create-category-dialog/index.tsx
@@ -12,9 +12,10 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useFetchAPI } from "../../hooks/useFetchAPI";
 
 export function CreateCategoryDialog() {
-    const { createCategory } = useFetchAPI()
+    const { createCategory, fetchCategories } = useFetchAPI();
     const [open, setOpen] = useState(false);
-    const { register, handleSubmit, formState } = useForm<CreateCategoryData>({
+
+    const { register, handleSubmit } = useForm<CreateCategoryData>({
         defaultValues: {
             title: '',
             color: theme.colors.primary,
@@ -28,20 +29,31 @@ export function CreateCategoryDialog() {
 
     const onSubmit = useCallback(
         async (data: CreateCategoryData) => {
-            await createCategory(data)
-            handleClose()
+            console.log("Dados enviados:", data);
+            await createCategory(data);
+            handleClose();
+            await fetchCategories();
         },
-        [handleClose, createCategory],
+        [handleClose, createCategory, fetchCategories],
     );
 
     return (
         <Dialog open={open} onOpenChange={setOpen} trigger={<Button>Nova categoria</Button>}>
             <Container>
-                <Title title='Nova Transação' subtitle='Crie uma nova categoria para suas transações' />
+                <Title title='Nova Categoria' subtitle='Crie uma nova categoria para suas transações' />
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <div>
-                        <Input label="Nome" placeholder="Nome da categoria..." />
-                        <Input label="Cor" type="color" style={{ cursor: "pointer" }} />
+                        <Input
+                            label="Nome"
+                            placeholder="Nome da categoria..."
+                            {...register("title")}
+                        />
+                        <Input
+                            label="Cor"
+                            type="color"
+                            style={{ cursor: "pointer" }}
+                            {...register("color")}
+                        />
                     </div>
                     <footer>
                         <Button onClick={handleClose} variant="outline" type="button">Cancelar</Button>
@@ -52,6 +64,3 @@ export function CreateCategoryDialog() {
         </Dialog>
     );
 }
-
-
-
