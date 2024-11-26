28441d18c0088676f179abceb232786e3aaad158
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-11-26 08:51:08 -0300

    feat: add createTransaction method to APIService for creating new transactions.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,16 @@
 import axios from "axios";
-import { Category, CreateCategory } from "./api-types";
+import { Category, CreateCategory, CreateTransaction, Transaction } from "./api-types";
 
 export class APIService {
     private static client = axios.create({
         baseURL: import.meta.env.VITE_API_URL
     })
+
+    static async createTransaction(createTransactionData: CreateTransaction): Promise<Transaction> {
+        const { data } = await APIService.client.post('/transactions', createTransactionData)
+
+        return data
+    }
 
     static async createCategory(createCategoryData: CreateCategory): Promise<Category> {
 
